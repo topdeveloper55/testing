f511f8f704dbeb04a56d5c3377ba28dd78f22490
nicolassoft94 <nicolas.soft94@gmail.com>
2023-10-27 01:36:44 +0900

    query

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -9,11 +9,11 @@ export const Profile = (props: any) => {
     <div className="space-y-8 max-w-md w-full border rounded-md px-8 py-10">
       <h2 className="text-xl">Profile Info</h2>
       <Avatar>
-        <AvatarImage src={props.update.avatar} alt="Avatar" />
+        <AvatarImage src={query.data?.avatar} alt="Avatar" />
       </Avatar>
       <div className="flex flex-col">
-        <Label>{props.update.name}</Label>
-        <Label>{props.update.description}</Label>
+        <Label>{query.data?.name}</Label>
+        <Label>{query.data?.description}</Label>
       </div>
     </div>
   );
